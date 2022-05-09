1ab778bbde3275bea55cf0ca4516c14863d65377
Melderey <mimeev2010@gmail.com>
2022-05-09 17:32:47 +0500

    Изменить роутинг

--- client/src/components/AppRouter/AppRouter.js
+++ client/src/components/AppRouter/AppRouter.js
@@ -1,12 +1,16 @@
 import { Route, Switch, Redirect } from "react-router-dom";
-import { CHAT_ROUTE, LOGIN_ROUTE } from "../../constants";
+import {
+  CHAT_ROUTE,
+  LOGIN_ROUTE,
+  MAIN_ROUTE,
+  OPERATOR_ROUTE,
+} from "../../constants";
 import UserChat from "../UserChat/UserChat";
 import Login from "../Login/Login";
+import OperatorChat from "../operatorChat/operatorChat";
 
 const AppRouter = () => {
-  const isAuth = true;
-
-  return isAuth ? (
+  return (
     <Switch>
       <Route
         key={CHAT_ROUTE}
@@ -14,19 +18,47 @@ const AppRouter = () => {
         component={UserChat}
         exact={true}
       />
-      <Redirect to={CHAT_ROUTE} />
-    </Switch>
-  ) : (
-    <Switch>
       <Route
         key={LOGIN_ROUTE}
         path={LOGIN_ROUTE}
         component={Login}
         exact={true}
       />
-      <Redirect to={LOGIN_ROUTE} />
+      <Route
+        key={OPERATOR_ROUTE}
+        path={OPERATOR_ROUTE}
+        component={OperatorChat}
+        exact={true}
+      />
+      <Redirect to={MAIN_ROUTE} />
     </Switch>
   );
 };
 
 export default AppRouter;
+
+// const AppRouter = () => {
+//   const isAuth = true;
+
+//   return isAuth ? (
+//     <Switch>
+//       <Route
+//         key={CHAT_ROUTE}
+//         path={CHAT_ROUTE}
+//         component={UserChat}
+//         exact={true}
+//       />
+//       <Redirect to={CHAT_ROUTE} />
+//     </Switch>
+//   ) : (
+//     <Switch>
+//       <Route
+//         key={LOGIN_ROUTE}
+//         path={LOGIN_ROUTE}
+//         component={Login}
+//         exact={true}
+//       />
+//       <Redirect to={LOGIN_ROUTE} />
+//     </Switch>
+//   );
+// };
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,4 @@
+export const MAIN_ROUTE = "/";
 export const CHAT_ROUTE = "/user_chat";
+export const OPERATOR_ROUTE = "/operator_chat";
 export const LOGIN_ROUTE = "/login";
